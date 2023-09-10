19dca4a741bc5e536ad5ea45ce23493434042dcd
sunshine-web-coder <webcoderdev02@gmail.com>
2023-09-10 12:19:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+// import {useEffect, useState} from 'react';
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import Home from './pages/home/Home';
